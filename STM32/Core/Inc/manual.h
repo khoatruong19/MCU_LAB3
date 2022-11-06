2dad88028c176b3f8acc281c3dfb2c407f6b5b5d
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2022-11-06 10:59:55 +0700

    Lab3

---
+++ STM32/Core/Inc/manual.h
@@ -0,0 +1,17 @@
+/*
+ * manual.h
+ *
+ *  Created on: Nov 3, 2022
+ *      Author: WELCOME
+ */
+
+#ifndef INC_MANUAL_H_
+#define INC_MANUAL_H_
+
+#include "global.h"
+#include "display_led.h"
+#include "button.h"
+
+void manual_run();
+
+#endif /* INC_MANUAL_H_ */
